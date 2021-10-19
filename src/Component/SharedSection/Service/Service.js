0b192cf971e85606d003abdceed64ce1747fe428
ahsanlisan <ahsanlisan2@.com>
2021-10-19 17:01:55 +0600

    3rd push

--- src/Component/SharedSection/Service/Service.js
+++ src/Component/SharedSection/Service/Service.js
@@ -1,19 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const Service = () => {
-  let { id } = useParams;
+  let { id } = useParams();
 
-  const [serviceDetail, setServiveDetail] = useState([]);
+  const [serviceDetails, setServiceDetails] = useState([]);
 
+  const [singleService, setSingleService] = useState({});
+
+  //  data load howa
   useEffect(() => {
-    fetch("./service.json")
+    fetch(
+      "https://raw.githubusercontent.com/Programming-Hero-Web-Course3/healthcare-related-website-ahsanlisan/master/public/service.json?token=AUU362FD5OMVRNACHK7CGHDBN2RPA"
+    )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setServiceDetails(data.service));
   }, []);
+
+  // call hbe jokon amar shob data load hye state e set hbe
+  useEffect(() => {
+    const foundService = serviceDetails.find((service) => service?.id === id);
+    setSingleService(foundService);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [serviceDetails]);
+
   return (
     <div>
-      <h2> : {id} </h2>
+      <h1>{id}</h1>
+      {console.log(singleService)}
+      {/* <h2> {singleService?.name}</h2> */}
+      <Container>
+        <Row>
+          <Col md={4}>
+            <Card>
+              <Card.Img variant="top" src={singleService?.imgurl} />
+            </Card>
+          </Col>
+          <Col md={5}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{singleService?.name}</Card.Title>
+                <Card.Text>
+                  <b> Email : {singleService?.shortintro}</b>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
